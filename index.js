0a4fd1cc6e2671dcef5f68636fff46c69e35020f
Edwin Peña <edwinsp095@gmail.com>
2025-04-25 22:33:41 -0500

    Remove cors from backend

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const requestLogger = (request, response, next) => {
 };
 
 app.use(express.json());
-app.use(cors());
+/* app.use(cors()); */
 app.use(express.static("dist"));
 app.use(requestLogger);
 
